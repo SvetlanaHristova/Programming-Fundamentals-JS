9ba2beb593e425ad66299d1b37825bd7e9dee13e
SvetlanaHristova <svetlanavhristova@gmail.com>
2022-09-05 15:56:47 +0300

    next unit

--- expressDemo.js/expressdemo.js
+++ expressDemo.js/expressdemo.js
@@ -1,34 +1,20 @@
 let express = require('express');
-
+let handlebars = require('express-handlebars').create;
+let catalogControlers = require('./controllers/catalogControlers');
 let app = express();
 
-let data = {
-    '0001': 'Oil filter',
-    '0002': 'Windshiel wiper',
-    '0003': 'Xenon headlght',
-    '0057': 'Spoiler'
-}
 
+app.engine('.hbs',handlebars({
+    extname: '.hbs',
+}).engine);
+app.set('view.engine', '.hbs');
 app.get('/', (req, res) => {
     res.send(`<h1>Home Page </h1>
     <a href="/catalog">Catalog </a>
     <p>Welcome to our site!`)
 });
-app.get('/catalog', (req, res) => {
-    res.send(`<h1> Catalog </h1>
-    <a href="/">Home </a>
-    <p> Lisr of product </p>
-        <ul>
-        ${Object.entries(data).map(e => `<li><a href="/catalog/${e[0]}">"${e[1]}"</a ></li >`).join('')}
-        </ul>`)
-});
-app.get(`/catalog/:productID`, (req, res) => {
-    let product = data[req.params.productID];
-    res.send(`<h1> Product Details </h1>
-<a href ="/catalog" > Back to Catalog </a>
-<p> Information about product</p><p>${product}</p>`);
-});
+app.get('/catalog', catalogControlers.catalog);
+app.get(`/catalog/:productID`, catalogControlers.details);
 
-app.listen(3000);
-
+app.listen(3000, () => console.log('Server listening on port 3000...'));
 
