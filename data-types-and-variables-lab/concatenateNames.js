63fedbc55d93256c73a741a10a48199df4d72df2
SvetlanaHristova <svetlanavhristova@gmail.com>
2022-09-23 16:12:08 +0300

    https://judge.softuni.org/Contests/Practice/Index/1242#1

---
+++ data-types-and-variables-lab/concatenateNames.js
@@ -0,0 +1,5 @@
+function concatenateNames(name, familyName, delimiter) {
+    console.log(name + delimiter + familyName);
+}
+concatenateNames('John', 'Smith', '->');
+concatenateNames('Jan', 'White', '<->');
